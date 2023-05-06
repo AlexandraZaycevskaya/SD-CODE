5561718d55673630a5acd82a4251467e9b829bdb
AlexandraZaycevskaya <126073816+AlexandraZaycevskaya@users.noreply.github.com>
2023-05-06 22:17:28 +0300

    Update index.js

--- counter-app/index.js
+++ counter-app/index.js
@@ -1,6 +1,6 @@
-let button = document.querySelector('.blue-button')
-let counterText = document.querySelector('.counter')
-let close = document.querySelector('.red-button')
+const button = document.querySelector('.blue-button')
+const counterText = document.querySelector('.counter')
+const resetButton = document.querySelector('.reset_button')
 let counter = 0;
 
 button.addEventListener('click' , function() {
@@ -9,7 +9,7 @@ button.addEventListener('click' , function() {
     counterText.innerText = counter;
 });
 
-close.addEventListener('click', function( ){
+resetButton.addEventListener('click', function( ){
     counter = 0;
 
     counterText.innerText = 0;
