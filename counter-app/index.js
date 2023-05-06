7ec33a99438d59a74c0ca841b3880f8c8bd2b901
AlexandraZaycevskaya <126073816+AlexandraZaycevskaya@users.noreply.github.com>
2023-05-06 22:36:06 +0300

    Update index.js

--- counter-app/index.js
+++ counter-app/index.js
@@ -1,9 +1,11 @@
-const button = document.querySelector('.blue-button')
+const addOneELement = document.getElementById('addOneElement')
 const counterText = document.querySelector('.counter')
-const resetButton = document.querySelector('.reset_button')
-let counter = 0;
+const resetButton = document.getElementById('resetElement')
+const COUNTER_INITIAL_VALUE = 0;
 
-button.addEventListener('click' , function() {
+let counter = CONTER_INITIAL_VALUE;
+
+addOneElement.addEventListener('click' , function() {
     counter = counter + 1;
 
     counterText.innerText = counter;
